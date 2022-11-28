8c0d68ac2b58cf34c1842efd2730ff000d2ea32f
Douglas Martin <dm12570@ms.wasatch.edu>
2022-11-28 09:55:37 -0700

    FormSite
    First portion if anything else needs to be changed

---
+++ functions.js
@@ -0,0 +1,17 @@
+
+
+function myFunction(){
+
+    window.alert("Thank You for Submitting this Survey");
+
+
+
+}
+
+function stylisticChange(){
+
+    var textBox = document.getElementById("something");
+    
+
+}
+
